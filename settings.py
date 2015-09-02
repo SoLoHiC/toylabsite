422c81471581a6e505ff962dbf298b02e0da1620
chuck <zimu.zhang@duitang.com>
2015-09-03 00:06:26 +0800

    remove local_setting info...

--- settings.py
+++ settings.py
@@ -167,11 +167,11 @@ DATABASES = {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.mysql",
         # DB name or path to database file if using sqlite3.
-        "NAME": "toylabsite",
+        "NAME": "",
         # Not used with sqlite3.
-        "USER": "root",
+        "USER": "",
         # Not used with sqlite3.
-        "PASSWORD": "root",
+        "PASSWORD": "",
         # Set to empty string for localhost. Not used with sqlite3.
         "HOST": "",
         # Set to empty string for default. Not used with sqlite3.
